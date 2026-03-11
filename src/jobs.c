wordgitet/neoash

commit 89a9a5407ce14818c0907629ce6e4cbd1f6b6a48

trap: fix interactive SIGINT and default trap handling
Fix the remaining interactive SIGINT and trap-default mismatches in the
yash POSIX signal matrix.

Preserve buffered top-level input across interrupted command loops so an
interactive shell fed from a file does not lose the next queued command
after SIGINT. Restore child signal state for interactive shells outside
background-job forks, and avoid re-signaling the parent shell when SIGINT
is trapped.

Add trap -p support and distinguish shell-default signal handling from
true signal default when trap explicitly restores a signal. Also preserve
nonzero status from fatal EXIT trap errors.

Areas:
  input: discard only the interrupted line instead of clearing all input
  jobs: reset child signal state and respect trapped SIGINT propagation
  trap: add trap -p and true-default signal restoration semantics

Signed-off-by: wordgitet <wordatet@linuxmail.org>

src/jobs.c
@@ -958,7 +958,7 @@ forkshell(struct job *jp, union node *n, int mode)
 			if (p->used)
 				freejob(p);
 		INTON;
-		if (wasroot && iflag) {
+		if (wasroot && iflag && mode != FORK_BG) {
 			setsignal(SIGINT);
 			setsignal(SIGQUIT);
 			setsignal(SIGTERM);
@@ -1106,7 +1106,7 @@ waitforjob(struct job *jp, int *signaled)
 		freejob(jp);
 	if (!int_pending()) {
 #if JOBS
-		if (rootshell && propagate_int &&
+		if (rootshell && propagate_int && !issigtrapped(SIGINT) &&
 			WIFSIGNALED(status) && WTERMSIG(status) == SIGINT)
 			kill(getpid(), SIGINT);
 #endif

src/trap.c
@@ -70,6 +70,7 @@
 
 static char sigmode[NSIG];	/* current value of signal */
 static int trap_force_signo;	/* trap is explicitly changing this signal */
+static int trap_default_signo;	/* trap is explicitly restoring true default */
 volatile sig_atomic_t pendingsig;	/* indicates some signal received */
 volatile sig_atomic_t pendingsig_waitcmd;	/* indicates wait builtin should be interrupted */
 volatile sig_atomic_t gotsigchld;	/* indicates a child status change was delivered */
@@ -149,6 +150,32 @@ printsignals(void)
 /*
  * The trap builtin.
  */
+static void
+printtrap(int signo, int show_default)
+{
+	const char *sign;
+
+	if (!show_default && trap[signo] == NULL)
+		return;
+
+	sign = signum_to_signame(signo);
+	out1str("trap -- ");
+	if (trap[signo] == NULL) {
+		if (show_default)
+			out1fmt("%d ", signo);
+		else
+			out1str("- ");
+	} else
+		out1qstr(trap[signo]);
+	if (signo == 0) {
+		out1str(" EXIT\n");
+	} else if (sign) {
+		out1fmt(" %s\n", sign);
+	} else {
+		out1fmt(" %d\n", signo);
+	}
+}
+
 int
 trapcmd(int argc __unused, char **argv)
 {
@@ -156,30 +183,43 @@ trapcmd(int argc __unused, char **argv)
 	int signo;
 	int errors = 0;
 	int i;
+	int print_only = 0;
+	int show_default = 0;
+	int force_default = 0;
 
-	while ((i = nextopt("l")) != '\0') {
+	while ((i = nextopt("lp")) != '\0') {
 		switch (i) {
 		case 'l':
 			printsignals();
 			return (0);
+		case 'p':
+			print_only = 1;
+			show_default = 1;
+			break;
 		}
 	}
 	argv = argptr;
 
+	if (print_only) {
+		if (*argv == NULL) {
+			for (signo = 0 ; signo < NSIG ; signo++)
+				printtrap(signo, show_default);
+			return 0;
+		}
+		for (; *argv; argv++) {
+			if ((signo = sigstring_to_signum(*argv)) == -1) {
+				warning("bad signal %s", *argv);
+				errors = 1;
+				continue;
+			}
+			printtrap(signo, show_default);
+		}
+		return errors;
+	}
+
 	if (*argv == NULL) {
 		for (signo = 0 ; signo < NSIG ; signo++) {
-			if (signo < NSIG && trap[signo] != NULL) {
-				const char *sign = signum_to_signame(signo);
-				out1str("trap -- ");
-				out1qstr(trap[signo]);
-				if (signo == 0) {
-					out1str(" EXIT\n");
-				} else if (sign) {
-					out1fmt(" %s\n", sign);
-				} else {
-					out1fmt(" %d\n", signo);
-				}
-			}
+			printtrap(signo, 0);
 		}
 		return 0;
 	}
@@ -191,6 +231,8 @@ trapcmd(int argc __unused, char **argv)
 			action = *argv;
 			argv++;
 		}
+	} else if (*argv) {
+		force_default = 1;
 	}
 	for (; *argv; argv++) {
 		if ((signo = sigstring_to_signum(*argv)) == -1) {
@@ -206,7 +248,10 @@ trapcmd(int argc __unused, char **argv)
 		trap[signo] = action;
 		if (signo != 0) {
 			trap_force_signo = signo;
+			if (force_default)
+				trap_default_signo = signo;
 			setsignal(signo);
+			trap_default_signo = 0;
 			trap_force_signo = 0;
 		}
 		INTON;
@@ -270,29 +315,31 @@ setsignal(int signo)
 	else
 		action = S_IGN;
 	if (action == S_DFL) {
-		switch (signo) {
-		case SIGINT:
-			action = S_CATCH;
-			break;
-		case SIGQUIT:
+		if (trap_default_signo != signo) {
+			switch (signo) {
+			case SIGINT:
+				action = S_CATCH;
+				break;
+			case SIGQUIT:
 #ifdef DEBUG
-			if (debug)
+				if (debug)
+					break;
+#endif
+				if (rootshell)
+					action = S_IGN;
+				break;
+			case SIGTERM:
+				if (rootshell)
+					action = S_IGN;
+				break;
+#if JOBS
+			case SIGTSTP:
+			case SIGTTOU:
+				if (rootshell && mflag)
+					action = S_IGN;
 				break;
 #endif
-			if (rootshell && iflag)
-				action = S_IGN;
-			break;
-		case SIGTERM:
-			if (rootshell && iflag)
-				action = S_IGN;
-			break;
-#if JOBS
-		case SIGTSTP:
-		case SIGTTOU:
-			if (rootshell && mflag)
-				action = S_IGN;
-			break;
-#endif
+			}
 		}
 	}
 	if (signo == SIGCHLD)
@@ -321,6 +368,8 @@ setsignal(int signo)
 			*t = S_RESET;	/* force to be set */
 		}
 	}
+	if (trap_default_signo == signo && (*t == S_HARD_IGN || *t == S_IGN))
+		*t = S_RESET;
 	if (*t == S_HARD_IGN && trap_force_signo == signo && iflag)
 		*t = S_RESET;
 	if (*t == S_HARD_IGN || *t == action)
@@ -370,10 +419,18 @@ ignoresig(int signo)
 
 
 int
+issigtrapped(int signo)
+{
+
+	return (trap[signo] != NULL && *trap[signo] != '\0');
+}
+
+
+int
 issigchldtrapped(void)
 {
 
-	return (trap[SIGCHLD] != NULL && *trap[SIGCHLD] != '\0');
+	return issigtrapped(SIGCHLD);
 }
 
 
@@ -539,6 +596,8 @@ exitshell_savedstatus(void)
 			FORCEINTON;
 			evalstring(p, 0);
 		}
+	} else if (exception != EXEXIT) {
+		exiting_exitstatus = exitstatus;
 	}
 	if (!setjmp(loc2.loc)) {
 		handler = &loc2;		/* probably unnecessary */

src/input.c
@@ -103,8 +103,17 @@ static void popstring(void);
 void
 resetinput(void)
 {
+	int c;
+
 	popallfiles();
-	parselleft = parsenleft = 0;	/* clear input buffer */
+	while (parsefile->strpush)
+		popstring();
+
+	c = PEOF;
+	if (parsefile->buf != NULL && parsenextc > parsefile->buf)
+		c = parsenextc[-1];
+	while (c != '\n' && c != PEOF && !int_pending())
+		c = pgetc();
 }
 
 void

src/trap.h
@@ -38,6 +38,7 @@ extern volatile sig_atomic_t gotsigchld;
 
 void clear_traps(void);
 int have_traps(void);
+int issigtrapped(int);
 void setsignal(int);
 void ignoresig(int);
 int issigchldtrapped(void);

commit 2260123d21858a61f1828ba885f596e865f42a25

trap: fix QUIT and TERM inheritance in child shells
Fix the remaining QUIT and TERM signal inheritance mismatches in the
yash signal matrix.

Limit the rootshell default-ignore policy for QUIT and TERM to
interactive rootshell cases, refresh QUIT only when rootshell
interactivity changes, and restore the expected QUIT or TERM state for
background child shells according to whether job control is active.

This keeps interactive exec from leaking ignored QUIT into execed
processes, preserves the expected ignore state for async +m exec paths,
and stops interactive background children from inheriting ignored TERM.

Areas:
  trap: refresh QUIT and TERM only in the rootshell cases that require it
  jobs: reset QUIT and TERM correctly for background child shells

Fixes: 89a9a5407ce14818c0907629ce6e4cbd1f6b6a48
Test: make -C test-posix/yash TESTEE=../../build/src/sh TESTS='sigterm5-p.tst sigterm7-p.tst sigstop7-p.tst bg-p.tst fg-p.tst job-p.tst wait-p.tst trap-p.tst' test-posix
Test: make -C test-posix/yash TESTEE=../../build/src/sh TIMEOUT=120s TESTS='sigquit7-p.tst sigquit8-p.tst' test-posix
Signed-off-by: wordgitet <wordatet@linuxmail.org>

src/jobs.c
@@ -958,9 +958,13 @@ forkshell(struct job *jp, union node *n, int mode)
 			if (p->used)
 				freejob(p);
 		INTON;
-		if (wasroot && iflag && mode != FORK_BG) {
-			setsignal(SIGINT);
-			setsignal(SIGQUIT);
+		if (wasroot && iflag) {
+			if (mode != FORK_BG) {
+				setsignal(SIGINT);
+				setsignal(SIGQUIT);
+			} else if (mflag) {
+				setsignal(SIGQUIT);
+			}
 			setsignal(SIGTERM);
 		}
 		return pid;

src/trap.c
@@ -325,11 +325,11 @@ setsignal(int signo)
 				if (debug)
 					break;
 #endif
-				if (rootshell)
+				if (rootshell && iflag)
 					action = S_IGN;
 				break;
 			case SIGTERM:
-				if (rootshell)
+				if (rootshell && iflag)
 					action = S_IGN;
 				break;
 #if JOBS
@@ -552,6 +552,8 @@ trap_init(void)
 void
 setinteractive(void)
 {
+	if (rootshell)
+		setsignal(SIGQUIT);
 	setsignal(SIGTERM);
 }
 