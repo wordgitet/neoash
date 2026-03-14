wordgitet/neoash

commit a5639b541ac53faa8ce26580f605aabdfc411d41

parser: tighten alias and quote handling
Improve parser and expansion behavior in a few POSIX-visible places that
showed up in the yash alias, quote, and case tests.

Handle aliases that expand to blank text without confusing the command
parser, allow reserved-word positions such as for/case keywords to be
recognized correctly around alias expansion, and defer alias-sensitive
command substitutions inside function bodies until execution time. Also
preserve escaped glob characters more faithfully in pattern contexts and
keep old-style parameter-expansion quote state aligned with yash.

Areas:
  alias: warn when unalias is asked to remove a missing name
  expand: preserve escaped pattern characters in glob and case paths
  input: only synthesize trailing alias blanks for aliases that end in blank

Test: make -C test-posix/yash TESTEE=../../build/src/sh TESTS='alias-p.tst quote-p.tst case-p.tst' test-posix
Signed-off-by: wordgitet <wordatet@linuxmail.org>

src/input.c
@@ -224,8 +224,13 @@ preadbuffer(void)
 		 * alias remains in use while parsing its last word.
 		 * This avoids alias recursions.
 		 */
-		if (parsenleft == -1 && parsefile->strpush->ap != NULL)
-			return ' ';
+		if (parsenleft == -1 && parsefile->strpush->ap != NULL) {
+			const char *val = parsefile->strpush->ap->val;
+			size_t len = strlen(val);
+
+			if (len != 0 && (val[len - 1] == ' ' || val[len - 1] == '\t'))
+				return ' ';
+		}
 		popstring();
 		if (--parsenleft >= 0)
 			return (*parsenextc++);

src/parser.c
@@ -55,6 +55,7 @@
 #include "show.h"
 #include "eval.h"
 #include "exec.h"	/* to check for special builtins */
+#include "jobs.h"
 #ifndef NO_HISTORY
 #include "myhistedit.h"
 #endif
@@ -102,6 +103,8 @@ static int quoteflag;		/* set if (part of) last token was quoted */
 static int startlinno;		/* line # where last token started */
 static int funclinno;		/* line # where the current function started */
 static struct parser_temp *parser_temp;
+static const char *const *checkkwd_override;
+static int suppressalias;
 
 #define NOEOFMARK ((const char *)&heredoclist)
 
@@ -126,6 +129,8 @@ static void synerror(const char *) __dead2;
 static void setprompt(int);
 static int pgetc_linecont(void);
 static void getusername(char *, size_t);
+static int isoverriddenkwd(const char *);
+static int isblankaliasval(const char *);
 
 
 static void *
@@ -194,6 +199,26 @@ parser_temp_free_all(void)
 	INTON;
 }
 
+static int
+isoverriddenkwd(const char *word)
+{
+	const char *const *pp;
+
+	for (pp = checkkwd_override; pp != NULL && *pp != NULL; pp++) {
+		if (equal(*pp, word))
+			return 1;
+	}
+	return 0;
+}
+
+static int
+isblankaliasval(const char *val)
+{
+	while (*val == ' ' || *val == '\t')
+		val++;
+	return *val == '\0';
+}
+
 
 /*
  * Read and parse a command.  Returns NEOF on end of file.  (NULL is a
@@ -223,6 +248,8 @@ parsecmd(int interact)
 	if (t == TEOF)
 		return NEOF;
 	if (t == TNL)
+		return NULL;
+	if (t == TBLANK)
 		return NULL;
 	tokpushback++;
 	return list(1);
@@ -468,20 +495,27 @@ command(void)
 		checkkwd = CHKKWD | CHKALIAS;
 		break;
 	case TFOR:
+		static const char *const for_in_kwd[] = { "in", "do", NULL };
+		static const char *const for_body_kwd[] = { "do", "{", NULL };
 		if (readtoken() != TWORD || quoteflag || ! goodname(wordtext))
 			synerror("Bad for loop variable");
 		n1 = (union node *)stalloc(sizeof (struct nfor));
 		n1->type = NFOR;
 		n1->nfor.var = wordtext;
-		checkkwd = CHKNL;
-		if (readtoken() == TWORD && !quoteflag &&
+		checkkwd = CHKNL | CHKALIAS;
+		checkkwd_override = for_in_kwd;
+		t = readtoken();
+		checkkwd_override = NULL;
+		if (t == TWORD && !quoteflag &&
 		    equal(wordtext, "in")) {
 			app = &ap;
+			suppressalias++;
 			while (readtoken() == TWORD) {
 				n2 = makename();
 				*app = n2;
 				app = &n2->narg.next;
 			}
+			suppressalias--;
 			*app = NULL;
 			n1->nfor.args = ap;
 			if (lasttoken == TNL)
@@ -505,8 +539,17 @@ command(void)
 			if (lasttoken != TSEMI)
 				tokpushback++;
 		}
-		checkkwd = CHKNL | CHKKWD | CHKALIAS;
-		if ((t = readtoken()) == TDO)
+		checkkwd = CHKNL | CHKALIAS;
+		checkkwd_override = for_body_kwd;
+		t = readtoken();
+		checkkwd_override = NULL;
+		if (t == TWORD && !quoteflag) {
+			if (equal(wordtext, "do"))
+				t = TDO;
+			else if (equal(wordtext, "{"))
+				t = TBEGIN;
+		}
+		if (t == TDO)
 			t = TDONE;
 		else if (t == TBEGIN)
 			t = TEND;
@@ -517,12 +560,17 @@ command(void)
 		checkkwd = CHKKWD | CHKALIAS;
 		break;
 	case TCASE:
+		static const char *const case_in_kwd[] = { "in", NULL };
+
 		n1 = (union node *)stalloc(sizeof (struct ncase));
 		n1->type = NCASE;
 		consumetoken(TWORD);
 		n1->ncase.expr = makename();
-		checkkwd = CHKNL;
-		if (readtoken() != TWORD || ! equal(wordtext, "in"))
+		checkkwd = CHKNL | CHKALIAS;
+		checkkwd_override = case_in_kwd;
+		t = readtoken();
+		checkkwd_override = NULL;
+		if (t != TWORD || ! equal(wordtext, "in"))
 			synerror("expecting \"in\"");
 		cpp = &n1->ncase.cases;
 		checkkwd = CHKNL | CHKKWD, readtoken();
@@ -590,6 +638,10 @@ command(void)
 		tokpushback++;
 		n1 = simplecmd(rpp, redir);
 		return n1;
+	case TBLANK:
+		if (redir)
+			synexpect(-1);
+		return NULL;
 	case TWORD:
 		tokpushback++;
 		n1 = simplecmd(rpp, redir);
@@ -723,7 +775,8 @@ makebinary(int type, union node *n1, union node *n2)
 void
 forcealias(void)
 {
-	checkkwd |= CHKALIAS;
+	if (!suppressalias)
+		checkkwd |= CHKALIAS;
 }
 
 void
@@ -822,6 +875,7 @@ readtoken(void)
 {
 	int t;
 	struct alias *ap;
+	int blankalias = 0;
 #ifdef DEBUG
 	int alreadyseen = tokpushback;
 #endif
@@ -846,6 +900,9 @@ readtoken(void)
 	{
 		const char * const *pp;
 
+		if (isoverriddenkwd(wordtext))
+			goto out;
+
 		if (checkkwd & CHKKWD)
 			for (pp = parsekwd; *pp; pp++) {
 				if (**pp == *wordtext && equal(*pp, wordtext))
@@ -855,12 +912,15 @@ readtoken(void)
 					goto out;
 				}
 			}
-		if (checkkwd & CHKALIAS &&
+		if (!suppressalias && (checkkwd & CHKALIAS) &&
 		    (ap = lookupalias(wordtext, 1)) != NULL) {
+			blankalias = isblankaliasval(ap->val);
 			pushstring(ap->val, strlen(ap->val), ap);
 			goto top;
 		}
 	}
+	if (blankalias && (t == TNL || t == TEOF))
+		lasttoken = t = TBLANK;
 out:
 	if (t != TNOT)
 		checkkwd = 0;
@@ -970,6 +1030,7 @@ struct tokenstate
 {
 	const char *syntax; /* *SYNTAX */
 	int parenlevel; /* levels of parentheses in arithmetic */
+	int oldvar_dq;
 	enum tokenstate_category
 	{
 		TSTATE_TOP,
@@ -1092,13 +1153,17 @@ parsebackq(char *out, struct nodelist **pbqlist,
 	size_t savelen;
 	const int bq_startlinno = plinno;
 	char *volatile ostr = NULL;
+	int suppress_bq_alias;
 	struct parsefile *const savetopfile = getcurrentfile();
 	struct heredoc *const saveheredoclist = heredoclist;
 	struct heredoc *here;
 
 	str = NULL;
+	suppress_bq_alias = 0;
 	if (setjmp(jmploc.loc)) {
 		popfilesupto(savetopfile);
+		if (suppress_bq_alias)
+			suppressalias--;
 		if (str)
 			ckfree(str);
 		if (ostr)
@@ -1184,8 +1249,29 @@ parsebackq(char *out, struct nodelist **pbqlist,
 			synexpect(-1);
 		doprompt = saveprompt;
 	} else {
+		char *cmdtext;
+		union node *wrapper;
+
+		if (funclinno != 0) {
+			suppressalias++;
+			suppress_bq_alias = 1;
+		}
 		n = list(0);
 		consumetoken(TRP);
+		if (suppress_bq_alias) {
+			suppressalias--;
+			suppress_bq_alias = 0;
+			INTOFF;
+			cmdtext = commandtext(n);
+			wrapper = (union node *)stalloc(sizeof(struct narg));
+			wrapper->type = NARG;
+			wrapper->narg.next = NULL;
+			wrapper->narg.backquote = NULL;
+			wrapper->narg.text = stsavestr(cmdtext);
+			ckfree(cmdtext);
+			INTON;
+			n = wrapper;
+		}
 	}
 
 	(*nlpp)->n = n;
@@ -1419,6 +1505,7 @@ readtoken1(int firstc, char const *initialsyntax, const char *eofmark,
 	level = 0;
 	state[level].syntax = initialsyntax;
 	state[level].parenlevel = 0;
+	state[level].oldvar_dq = 0;
 	state[level].category = TSTATE_TOP;
 
 	STARTSTACKSTR(out);
@@ -1430,6 +1517,9 @@ readtoken1(int firstc, char const *initialsyntax, const char *eofmark,
 			CHECKSTRSPACE(4, out);	/* permit 4 calls to USTPUTC */
 
 			synentry = state[level].syntax[c];
+			if (state[level].category == TSTATE_VAR_OLD &&
+			    state[level].oldvar_dq && c == '\'')
+				synentry = CWORD;
 
 			switch(synentry) {
 			case CNL:	/* '\n' */
@@ -1591,8 +1681,10 @@ endword:
 	if (eofmark == NULL) {
 		if ((c == '>' || c == '<')
 		 && quotef == 0
-		 && len <= 2
-		 && (*out == '\0' || is_digit(*out))) {
+		 && ((len <= 2 && (*out == '\0' || is_digit(*out))) ||
+		     (len == 1 && out[0] == '<' && c == '>'))) {
+			if (len == 1 && out[0] == '<')
+				*out = '\0';
 			parseredir(out, c);
 			return lasttoken = TREDIR;
 		} else {
@@ -1764,6 +1856,7 @@ varname:
 			}
 			level++;
 			state[level].parenlevel = 0;
+			state[level].oldvar_dq = 0;
 			if (subtype == VSMINUS || subtype == VSPLUS ||
 			    subtype == VSQUESTION || subtype == VSASSIGN) {
 				/*
@@ -1771,6 +1864,8 @@ varname:
 				 * inherit the double-quote state.
 				 */
 				state[level].syntax = state[level - 1].syntax;
+				state[level].oldvar_dq =
+				    state[level - 1].syntax == DQSYNTAX;
 				state[level].category = TSTATE_VAR_OLD;
 			} else {
 				/*

src/alias.c
@@ -236,7 +236,10 @@ unaliascmd(int argc __unused, char **argv __unused)
 		}
 	}
 	for (i = 0; *argptr; argptr++)
-		i |= unalias(*argptr);
+		if (unalias(*argptr)) {
+			warning("%s: not found", *argptr);
+			i = 1;
+		}
 
 	return (i);
 }

src/eval.c
@@ -644,6 +644,7 @@ evalbackcmd(union node *n, struct backcmd *result)
 	struct jmploc *savehandler;
 	struct localvar *savelocalvars;
 	unsigned char saveoptreset;
+	int deferred;
 
 	result->fd = -1;
 	result->buf = NULL;
@@ -655,7 +656,9 @@ evalbackcmd(union node *n, struct backcmd *result)
 	}
 	setstackmark(&smark);
 	exitstatus = oexitstatus;
-	if (is_valid_fast_cmdsubst(n)) {
+	deferred = n->type == NARG && n->narg.next == NULL &&
+	    n->narg.backquote == NULL;
+	if (!deferred && is_valid_fast_cmdsubst(n)) {
 		savelocalvars = localvars;
 		localvars = NULL;
 		saveoptreset = shellparam.reset;
@@ -692,7 +695,10 @@ evalbackcmd(union node *n, struct backcmd *result)
 				dup2(pip[1], 1);
 				close(pip[1]);
 			}
-			evaltree(n, EV_EXIT);
+			if (deferred)
+				evalstring(n->narg.text, EV_EXIT);
+			else
+				evaltree(n, EV_EXIT);
 		}
 		close(pip[1]);
 		result->fd = pip[0];

src/expand.c
@@ -160,6 +160,26 @@ stputs_quotes(const char *data, const char *syntax, char *p)
 #define STPUTS_QUOTES(data, syntax, p) p = stputs_quotes((data), syntax, p)
 
 static char *
+stputs_pattern(const char *data, char *p)
+{
+	char c;
+
+	while ((c = *data++) != '\0') {
+		CHECKSTRSPACE(3, p);
+		if (c == '\\' && *data != '\0') {
+			USTPUTC(CTLESC, p);
+			USTPUTC(*data++, p);
+			continue;
+		}
+		if (BASESYNTAX[(int)c] == CCTL)
+			USTPUTC(CTLESC, p);
+		USTPUTC(c, p);
+	}
+	return (p);
+}
+#define STPUTS_PATTERN(data, p) p = stputs_pattern((data), p)
+
+static char *
 nextword(char c, int flag, char *p, struct worddest *dst)
 {
 	int is_ws;
@@ -192,10 +212,17 @@ stputs_split(const char *data, const char *syntax, int flag, char *p,
 
 	ifs = ifsset() ? ifsval() : " \t\n";
 	while (*data) {
-		CHECKSTRSPACE(2, p);
+		CHECKSTRSPACE(3, p);
 		c = *data++;
 		if (strchr(ifs, c) != NULL) {
 			NEXTWORD(c, flag, p, dst);
+			continue;
+		}
+		if ((flag & EXP_GLOB) && c == '\\' &&
+		    (*data == '*' || *data == '?' || *data == '[')) {
+			USTPUTC('\\', p);
+			USTPUTC(CTLESC, p);
+			USTPUTC(*data++, p);
 			continue;
 		}
 		if (flag & EXP_GLOB && syntax[(int)c] == CCTL)
@@ -874,8 +901,12 @@ strtodest(const char *p, int flag, int subtype, int quoted,
 		STPUTS(p, expdest);
 	else if (flag & EXP_SPLIT && !quoted && dst != NULL)
 		STPUTS_SPLIT(p, BASESYNTAX, flag, expdest, dst);
-	else if (flag & (EXP_GLOB | EXP_CASE))
-		STPUTS_QUOTES(p, quoted ? DQSYNTAX : BASESYNTAX, expdest);
+	else if (flag & (EXP_GLOB | EXP_CASE)) {
+		if (quoted)
+			STPUTS_QUOTES(p, DQSYNTAX, expdest);
+		else
+			STPUTS_PATTERN(p, expdest);
+	}
 	else
 		STPUTS(p, expdest);
 }
@@ -1036,6 +1067,10 @@ expandmeta(char *pattern, struct arglist *dstlist)
 	firstmatch = dstlist->count;
 	p = pattern;
 	for (; (c = *p) != '\0'; p++) {
+		if (c == CTLESC && p[1] != '\0') {
+			p++;
+			continue;
+		}
 		/* fast check for meta chars */
 		if (c == '*' || c == '?' || c == '[') {
 			INTOFF;

src/mktokens
@@ -51,6 +51,7 @@ TENDCASE 1	";;"
 TFALLTHRU 1	";&"
 TREDIR	0	redirection
 TWORD	0	word
+TBLANK	0	blank
 TIF	0	"if"
 TTHEN	1	"then"
 TELSE	1	"else"

commit 04c466cd3fa051a2c27059615208c16a4c94479d

input: defer non-blank alias release until next token
Keep ALIASINUSE set across the token boundary for aliases whose
values do not end in blank or tab. This prevents a self-referential
alias like `alias command=command` from being re-expanded while
`eval` reparses the next word, which is what wedged
`parser/alias13.0`.

Blank-ending aliases still clear immediately on pop because they already
use the synthetic trailing-space rule to keep the alias active through
its last word. Delaying those aliases as well breaks yash cases for
blank-ending and recursive alias expansion.

Areas:
  input: track delayed alias clears until the next token boundary
  parser: release delayed aliases at readtoken entry

Fixes: a5639b541ac53faa8ce26580f605aabdfc411d41
Signed-off-by: wordgitet <wordatet@linuxmail.org>

src/input.c
@@ -83,6 +83,11 @@ struct parsefile {
 	struct strpush basestrpush; /* so pushing one is fast */
 };
 
+struct aliasclear {
+	struct aliasclear *next;
+	struct alias *ap;
+};
+
 
 int plinno = 1;			/* input line number */
 int parsenleft;			/* copy of parsefile->nleft */
@@ -94,11 +99,13 @@ static struct parsefile basepf = {	/* top level input file */
 	.buf = basebuf
 };
 static struct parsefile *parsefile = &basepf;	/* current input file */
+static struct aliasclear *delayedaliases;
 int whichprompt;		/* 1 == PS1, 2 == PS2 */
 
 static void pushfile(void);
 static int preadfd(void);
 static void popstring(void);
+void cleardelayedaliases(void);
 
 void
 resetinput(void)
@@ -106,6 +113,7 @@ resetinput(void)
 	int c;
 
 	popallfiles();
+	cleardelayedaliases();
 	while (parsefile->strpush)
 		popstring();
 
@@ -355,16 +363,37 @@ pushstring(const char *s, int len, struct alias *ap)
 }
 
 static void
+delayaliasclear(struct alias *ap)
+{
+	struct aliasclear *ac;
+
+	ac = malloc(sizeof(*ac));
+	if (ac == NULL) {
+		ap->flag &= ~ALIASINUSE;
+		return;
+	}
+	ac->ap = ap;
+	ac->next = delayedaliases;
+	delayedaliases = ac;
+}
+
+static void
 popstring(void)
 {
 	struct strpush *sp = parsefile->strpush;
 
 	INTOFF;
 	if (sp->ap) {
+		const char *val = sp->ap->val;
+		size_t len = strlen(val);
+
 		if (parsenextc != sp->ap->val &&
 		    (parsenextc[-1] == ' ' || parsenextc[-1] == '\t'))
 			forcealias();
-		sp->ap->flag &= ~ALIASINUSE;
+		if (len != 0 && (val[len - 1] == ' ' || val[len - 1] == '\t'))
+			sp->ap->flag &= ~ALIASINUSE;
+		else
+			delayaliasclear(sp->ap);
 	}
 	parsenextc = sp->prevstring;
 	parsenleft = sp->prevnleft;
@@ -374,6 +403,18 @@ popstring(void)
 	if (sp != &(parsefile->basestrpush))
 		ckfree(sp);
 	INTON;
+}
+
+void
+cleardelayedaliases(void)
+{
+	struct aliasclear *ac;
+
+	while ((ac = delayedaliases) != NULL) {
+		delayedaliases = ac->next;
+		ac->ap->flag &= ~ALIASINUSE;
+		free(ac);
+	}
 }
 
 /*
@@ -534,6 +575,7 @@ popallfiles(void)
 {
 	while (parsefile != &basepf)
 		popfile();
+	cleardelayedaliases();
 }
 
 

src/input.h
@@ -59,5 +59,6 @@ void popfilesupto(struct parsefile *);
 void popallfiles(void);
 void closescript(void);
 void flushinput(void);
+void cleardelayedaliases(void);
 
 #define pgetc_macro()	(--parsenleft >= 0? *parsenextc++ : preadbuffer())

src/parser.c
@@ -877,6 +877,7 @@ readtoken(void)
 #endif
 
 	top:
+	cleardelayedaliases();
 	t = xxreadtoken();
 
 	/*